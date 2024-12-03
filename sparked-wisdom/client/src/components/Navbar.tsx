91eaa5ff30e11bd9d5337408748049b4be129381
Nic Nic Miller <nicmiller13@gmail.com>
2024-12-02 22:11:27 -0500

    added to navbar and set up savedquotes file

--- sparked-wisdom/client/src/components/Navbar.tsx
+++ sparked-wisdom/client/src/components/Navbar.tsx
@@ -40,6 +40,16 @@ const Navbar = () => {
         
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/savedquotes">
+                Saved quotes
+              </Link>
+            </li>
             {!loginCheck ? (
               <li className="nav-item">
                 <Link className="nav-link" to="/login">
